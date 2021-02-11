f75f9c0191909d8c712b6dbf114d62e9adc07c5a
IshanJawade <ishankjawade@gmail.com>
2021-02-11 20:00:35 +0530

    Basic UI is Ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div clasName='main-container'>
+      <div className='box'>
+        <div className='header'>
+          Currency Convertor
+        </div>
+        <div className='colunm-one'>
+          <div className='title'> Select Currency</div>
+          <div className='drop-down1'>
+            <select id="cars" name="cars">
+              <option value="volvo">INT</option>
+              <option value="saab">USD</option>
+              <option value="fiat">EUR</option>
+              <option value="audi">DNR</option>
+            </select>
+            <div className="input1">
+              <input 
+                type='text' 
+                className='search-bar'
+                placeholder='Convert from ...'
+              />
+            </div>
+          </div>
+      </div>
+
+      <div className='colunm-two'>
+        <div className='title'> Select Currency</div>
+        <div className='drop-down2'>
+            <select id="cars" name="cars">
+              <option value="volvo">INT</option>
+              <option value="saab">USD</option>
+              <option value="fiat">EUR</option>
+              <option value="audi">DNR</option>
+            </select>
+        </div>
+        <div className="input2">
+              <input 
+                type='text' 
+                className='search-bar'
+                placeholder='Converted intox...'
+              />
+        </div>
+      </div>
+      <div className='footer'>
+        <footer >
+        © Random Shit Productions
+        </footer>
+      </div>
     </div>
+  </div>  
   );
 }
 
